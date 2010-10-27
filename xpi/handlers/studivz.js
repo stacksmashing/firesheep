ff9272abd52298360f3a9387ba667f1aaff04edb
Thomas (nezza-_-) Roth <code@stacksmashing.net>
2010-10-27 21:57:12 +0200

    German social networks: SchuelerVZ and StudiVZ handlers.

---
+++ xpi/handlers/studivz.js
@@ -0,0 +1,16 @@
+register({
+        name: 'StudiVZ',
+        url: 'http://www.studivz.net/Home',
+        domains: ['studivz.net'],
+        sessionCookieNames: ['s_sess', 's_pers','vz-status'],
+        processPacket: function () {
+            var s_sess = this.firstPacket.cookies['s_sess'];
+            this.sessionId = s_sess;
+        },
+        identifyUser: function() {
+            var resp = this.httpGet("http://www.studivz.net/Home");
+            var t = resp.body.querySelector('#comment-user-data');
+            this.userName = t.getElementsByTagName('input')[0].value;
+            this.userAvatar = t.getElementsByTagName('input')[3].value;        
+        }       
+});
